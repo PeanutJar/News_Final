a33154a6b15498581598806947638e051d0fe890
PeanutJar <dexpaclebar@gmail.com>
2025-08-16 17:34:12 -0700

    Loading Animation
    added various imports/dependencies (most notably usestate, use effect, and the lottie support). Main feature is now playing a 'news' loading animation that prevents display of other elements until the animation is finished

--- News_Final/app/(tabs)/index.tsx
+++ News_Final/app/(tabs)/index.tsx
@@ -1,14 +1,41 @@
 import { Text, SafeAreaView, StyleSheet, View, TextInput} from 'react-native';
 
+import React, { useState, useEffect } from 'react';
+import Animated from 'react-native-reanimated';
+import LottieView from 'lottie-react-native';
+import filter from "lodash.filter";
+
 export default App;
 
 function App() {
+  const [anim, setAnimation] = useState(true); //usestate for animation
+
+  //renders after first instance of 
+  useEffect(() => {
+   
+  },[])
+
   return (
     <SafeAreaView style={styles.container}>
-      <View style={{position:'absolute',top:0,backgroundColor: '#80B1DE', width:'110%',height:'4%'}}></View>
-      <View style={styles.headerbox}> //news header
-        <Text style={styles.headertext}> NEWS </Text>
-      </View>
+
+      {(anim) ? (
+        <LottieView style={{width:300, height:300}}
+        source={require("../../assets/News.json")}
+        autoPlay
+        loop={false}
+        resizeMode='cover'
+        onAnimationFinish={() => setAnimation(false)}
+        />
+      ) : (
+        //"<>" allows for multiple objects to be entered
+        <> 
+        <View style={{position:'absolute',top:0,backgroundColor: '#80B1DE', width:'110%',height:'4%'}}></View>
+        <View style={styles.headerbox}> //news header
+          <Text style={styles.headertext}> NEWS </Text>
+        </View>
+      
+       </>
+      )}
     </SafeAreaView>
   );
 }
